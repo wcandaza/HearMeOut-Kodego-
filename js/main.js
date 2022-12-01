505fddced08bc8e38e51007f1273fee47461bfc0
wcandaza <winfred.candaza@live.com>
2022-12-01 16:55:44 +0800

    added chat and time capsule js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 //=================================chat===========================================//
+///////enabled chat window
+function enableChatWindow() {
+    let volunteerContainer = document.querySelector("#volunteer-container");
+    volunteerContainer.style.display= "none";
+    let chatWindow = document.querySelector("#chat-window");
+    chatWindow.style.display= "block"
+}
+
+///////end of enabled chat window
 
 let chatContainerUser = document.querySelector("#chatContainerUser");
 let inputChatUser = document.querySelector("#inputChatUser");
@@ -6,8 +15,11 @@ let inputChatUser = document.querySelector("#inputChatUser");
 //local storage key increment
 let key = localStorage.getItem("incrementing key");
 
+//get key from local storage
+let newKey = "";
+
 //render chat message on landing
-let x = localStorage.key("incrementing key")
+// let x = localStorage.key("incrementing key")
 // loadchatMessages();
 
 function sendMessageFunctionUser() {
@@ -29,8 +41,7 @@ function loadchatMessages() {
     //add attribute to user's chat input
     chatDivUser.classList = "userChatDiv";
     //display message on chat box
-    let x = "chatInputUser" + parseFloat(key);
-    let y = parseFloat(x);
+    let x = "chatInputUser" + key;
     console.log("this is the key " + x)
     let getChatLocalStorage = localStorage.getItem(x);
     let displayUserChat = "User: " + getChatLocalStorage;
@@ -51,12 +62,26 @@ const timeCapsuleStoreBtn = document.querySelector("#timeCapsuleStoreBtn");
 timeCapsuleStoreBtn.addEventListener("click", storeFunction);
 function storeFunction() {
     const enteredDate = document.querySelector("#date").value;
+    // store to local
+    localStorage.setItem("inputDate", enteredDate)
+    let currentdate = new Date();
+    localStorage.setItem("currentDate", currentdate)
+    // get stored data
+    let getDate = localStorage.getItem("inputDate")
+    let getCurrentDate = localStorage.getItem("currentDate")
     date1 = new Date(enteredDate)
     date2 = new Date()
-    result = Math.abs(date1-date2)
+    result = date1-date2
+    if (result >= 86400000){
+        console.log("cannot open the capsule")
+    } else {
+        console.log("you can open the capsule")
+    }
     console.log(date1)
     console.log(date2)
     console.log(result)
-
-
+    console.log(getDate)
+    console.log(getCurrentDate)
+    console.log(getDate - getCurrentDate)
+    
 }
